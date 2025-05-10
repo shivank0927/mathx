d1fb6c81ee80f483977a49076dbe2ef070a93d01
shivank0927 <shivang.arya09@gmail.com>
2025-05-10 13:11:35 +0530

    will be back after learning memory management

--- strassens matrix multiplication algorithm/strassens.c
+++ strassens matrix multiplication algorithm/strassens.c
@@ -2,25 +2,49 @@
 #include <stdlib.h>
 #include <time.h>
 
+// strassen's matrix multiplication
+
+
 int main(void){
     srand(time(NULL))
 
-    int MatrixOne = [8][8];
-    int MatrixTwo = [8][8];
-    
+    int MatrixOne[4][4];
+    int MatrixTwo[4][4];
+
     // init
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
             MatrixOne[i][j] = rand() % 100 + 1; // generate random val. b/w 1 - 100
         }
     }
     
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
             MatrixTwo[j] = rand() % 100 + 1; // generate random val. b/w 1 - 100
         }
     }
 
+    int dimension = 4; // exp. on 4x4 matrix for now
+
+    int result = recurse(MatrixOne, MatrixTwo, dimension);
+
+    printf("result: \n\n\n\n%10d", result);
 
     return 0;
+}
+
+
+int recurse(int MatrixOne, int MatrixTwo, int dimension){
+
+    if (dimension <= 2){
+        // c = 4 formulas
+    }else{
+        mid = dimension / 2;
+
+    }
+
+
+
+
+
 }
